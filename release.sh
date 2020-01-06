b71c05896fe9587434cf256dd4769affb28c530b
Regis Leray <r.leray.byd@b-yond.com>
2020-01-06 00:30:24 -0500

    dont encode in base64

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 set -e
 git fetch --tags
-echo -n "$PGP_PUBLIC" | base64 -d > /tmp/public.asc
-echo -n "$PGP_SECRET" | base64 -d > /tmp/secret.asc
+echo -n "$PGP_PUBLIC" > /tmp/public.asc
+echo -n "$PGP_SECRET" > /tmp/secret.asc
 echo "credentials += Credentials(\"Sonatype Nexus Repository Manager\", \"oss.sonatype.org\", \"$SONATYPE_USER\", \"$SONATYPE_PASSWORD\")" > ~/.sbt/1.0/sonatype.sbt
 sbt ++$TRAVIS_SCALA_VERSION! clean sonatypeBundleClean +publishSigned sonatypeBundleRelease
