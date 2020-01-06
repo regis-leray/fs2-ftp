665bd1d599925c2563c9077637200ae97977bc1b
Regis Leray <r.leray.byd@b-yond.com>
2020-01-06 08:20:17 -0500

    release

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-git fetch --tags
-echo -n "$PGP_PUBLIC" > /tmp/public.asc
-echo -n "$PGP_SECRET" > /tmp/secret.asc
-echo "credentials += Credentials(\"Sonatype Nexus Repository Manager\", \"oss.sonatype.org\", \"$SONATYPE_USER\", \"$SONATYPE_PASSWORD\")" > ~/.sbt/1.0/sonatype.sbt
-sbt ++$TRAVIS_SCALA_VERSION! clean sonatypeBundleClean +publishSigned sonatypeBundleRelease
