31236c47f2adcc272f9419d37838d3a72da44d63
Regis Leray <regis.leray@gmail.com>
2022-01-07 20:33:21 -0500

    UnsecureFtp doesn't download properly (#198)

--- src/main/scala/fs2/ftp/errors.scala
+++ src/main/scala/fs2/ftp/errors.scala
@@ -5,11 +5,13 @@ import java.io.IOException
 /**
  * Represent a connection failure to a FTP/SFTP servers
  */
-case class ConnectionError(message: String, cause: Throwable) extends IOException(message, cause)
+final case class ConnectionError(message: String, cause: Throwable) extends IOException(message, cause)
 
 object ConnectionError {
   def apply(message: String): ConnectionError  = new ConnectionError(message, new Throwable(message))
   def apply(cause: Throwable): ConnectionError = new ConnectionError(cause.getMessage, cause)
 }
 
-case class InvalidPathError(message: String) extends IOException(message)
+final case class InvalidPathError(message: String) extends IOException(message)
+
+final case class FileTransferIncompleteError(message: String) extends IOException(message)
